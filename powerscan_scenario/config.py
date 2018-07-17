21e5d1221015ddc728d647343fa84062bd1d66f4
jssuzanne <jssuzanne@anybox.fr>
2018-07-17 18:42:30 +0200

    Add Configuration dict

--- powerscan_scenario/config.py
+++ powerscan_scenario/config.py
@@ -7,43 +7,81 @@
 # obtain one at http://mozilla.org/MPL/2.0/.
 from pkg_resources import iter_entry_points
 from logging import getLogger
+import os
+from os.path import isfile
+from configparser import ConfigParser
 logger = getLogger(__name__)
 
 
-def get_argparse_config_from_entrypoint(parser, entry_point):
+class ConfigurationException(Exception):
+    pass
+
+
+class Configuration(dict):
+
+    configfile_section = None
+
+    def set_configfile_section(self, section):
+        self.configfile_section = section
+
+    def parse_configfile(self, configfile):
+        if self.configfile_section is None:
+            raise ConfigurationException("No configfile section name")
+
+        if not isfile(configfile):
+            raise ConfigurationException(
+                "No such file or not a regular file: %r " % configfile)
+
+        cparser = ConfigParser()
+        cparser.read(configfile)
+        sections = cparser.items(self.configfile_section)
+        for opt, value in sections:
+            self[opt] = value
+
+    def update(self, dict2):
+        if 'configfile' in dict2:
+            configfile = os.path.abspath(dict2['configfile'])
+            self.parse_configfile(configfile)
+
+        super(Configuration, self).update(dict2)
+
+
+def get_argparse_config_from_entrypoint(parser, entry_point,
+                                        default_configuration):
     logger.info('powerscan_scenario load %r configuration' % entry_point)
     for i in iter_entry_points(entry_point):
-        i.load()(parser)
+        i.load()(parser, default_configuration)
 
 
-def add_logging_argparse_config(parser):
+def add_logging_argparse_config(parser, default_configuration):
     group = parser.add_argument_group('Powerscan logging')
     group.add_argument('--logging-level', dest='logging_level',
                        choices=['NOTSET', 'DEBUG', 'INFO', 'WARNING',
-                                'ERROR', 'CRITICAL'], default='INFO')
+                                'ERROR', 'CRITICAL'])
     group.add_argument('--logging-configfile', dest='logging_configfile',
                        help="Relative path of the logging config file")
+    default_configuration.update(dict(logging_level='INFO'))
 
 
-def add_scenario_argparse_config(parser):
+def add_scenario_argparse_config(parser, default_configuration):
     group = parser.add_argument_group('Powerscan scenario')
     group.add_argument('-c', '--configfile', dest='configfile',
                        help="Relative path of the config file")
     group.add_argument('-p', '--serial-port', dest='serial_port',
-                       default='/dev/ttyUSB0',
                        help="Serial port used by the scanner")
     group.add_argument('-u', '--sqlalchemy-url', dest='sqlalchemy_url',
                        help="SQLAlchemy url to connect to the database")
+    default_configuration.update(dict(serial_port='/dev/ttyUSB0'))
 
 
-def add_config_argparse_config(parser):
+def add_config_argparse_config(parser, default_configuration):
     group = parser.add_argument_group('Powerscan config')
     group.add_argument('-c', '--configfile', dest='configfile',
                        help="Relative path of the config file")
     group.add_argument('-p', '--serial-port', dest='serial_port',
-                       default='/dev/ttyUSB0',
                        help="Serial port used by the scanner")
     group.add_argument('-s', '--scanner-code', dest='scanner_code',
                        help="Code of the scanner")
     group.add_argument('-k', '--scanner-configfile', dest='scanner_configfile',
                        help="Relative path of the config file for the scanner")
+    default_configuration.update(dict(serial_port='/dev/ttyUSB0'))
--- powerscan_scenario/tests/test_argparse_configuration.py
+++ powerscan_scenario/tests/test_argparse_configuration.py
@@ -6,8 +6,9 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
 from unittest import TestCase
+import tempfile
 from ..config import (
-    add_scenario_argparse_config, add_config_argparse_config,
+    add_scenario_argparse_config, add_config_argparse_config, Configuration,
     add_logging_argparse_config, get_argparse_config_from_entrypoint
 )
 
@@ -41,20 +42,46 @@ class TestScenarioArgParse(GiveParser, TestCase):
 
     def test_get_config_from_entry_point(self):
         get_argparse_config_from_entrypoint(
-            self.parser, 'powerscan_scenario.argparse')
+            self.parser, 'powerscan_scenario.argparse', {})
+
+    def test_configuration(self):
+        conf = Configuration()
+        conf.set_configfile_section('POWERSCAN_SCENARIO')
+        dict2 = {'serial_port': '/dev/ttyUSB0'}
+        conf.update(dict2)
+        self.assertEqual(conf, dict2)
+
+    def test_configuration_with_configfile(self):
+        conf = Configuration()
+        conf.set_configfile_section('POWERSCAN_SCENARIO')
+        with tempfile.NamedTemporaryFile('w') as fp:
+            fp.write('[POWERSCAN_SCENARIO]\nserial_port = /dev/ttyUSB0')
+            fp.seek(0)
+            conf.update(dict(configfile=fp.name))
+            self.assertEqual(conf, {
+                'configfile': fp.name,
+                'serial_port': '/dev/ttyUSB0'})
 
     def test_add_scenario_argparse_config(self):
-        add_scenario_argparse_config(self.parser)
+        default_configuration = {}
+        add_scenario_argparse_config(self.parser, default_configuration)
+        self.assertEqual(default_configuration,
+                         {'serial_port': '/dev/ttyUSB0'})
 
     def test_add_logging_argparse_config(self):
-        add_logging_argparse_config(self.parser)
+        default_configuration = {}
+        add_logging_argparse_config(self.parser, default_configuration)
+        self.assertEqual(default_configuration, {'logging_level': 'INFO'})
 
 
 class TestConfigArgParse(GiveParser, TestCase):
 
     def test_get_config_from_entry_point(self):
         get_argparse_config_from_entrypoint(
-            self.parser, 'powerscan_config.argparse')
+            self.parser, 'powerscan_config.argparse', {})
 
     def test_add_config_argparse_config(self):
-        add_config_argparse_config(self.parser)
+        default_configuration = {}
+        add_config_argparse_config(self.parser, default_configuration)
+        self.assertEqual(default_configuration,
+                         {'serial_port': '/dev/ttyUSB0'})
